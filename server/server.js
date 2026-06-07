fbbe1d554631cb4c57b806ab6c2949e86db26134
CHANA BELENSON <chanibelenson@gmail.com>
2026-06-07 17:41:22 +0300

    update task is working, baruch-hashem

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174'], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(logger);
